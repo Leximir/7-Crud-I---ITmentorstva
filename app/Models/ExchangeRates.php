ea741221b4c60ccbec3f91e41c4006716d581791
Leximir <aleksa.stankovic@proton.me>
2025-02-22 21:23:35 +0100

    Dodana komanda da vidi postoji li valuta za danasji dan

--- app/Models/ExchangeRates.php
+++ app/Models/ExchangeRates.php
@@ -3,9 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class ExchangeRates extends Model
 {
+    const CURRENCY_EUR = 'eur';
+    const CURRENCY_USD = 'usd';
+    const CURRENCY_RUB = 'rub';
+    const CURRENCY_BAM = 'bam';
+    const AVAILABLE_CURRENCIES = [
+        self::CURRENCY_RUB, self::CURRENCY_BAM, self::CURRENCY_EUR, self::CURRENCY_USD
+    ];
+
     protected $table = 'exchange_rates';
     protected $fillable = ['currency', 'value'];
+
+    public static function getCurrencyForToday($currency){
+        return self::where('currency', $currency)
+            ->whereDate('created_at', Carbon::now())
+            ->first();
+    }
 }
