e9d9a5d0bb68ed77410a63d7195bb6e773cf6e85
Leximir <aleksa.stankovic@proton.me>
2025-02-23 21:52:47 +0100

    Odradjen domaci za 22 predavanje

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,29 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-    Route::get("/", [HomepageController::class , 'index']);
+Route::get("/", [HomepageController::class , 'index']);
 
-    Route::get("about" , [AboutController::class , 'index']);
+Route::get("about" , [AboutController::class , 'index']);
 
-    Route::get("/shop" , [ProductsController::class , 'indexClient']);
+Route::get("/shop" , [ProductsController::class , 'indexClient']);
 
-    Route::controller(ContactController::class)->name('contact')->group(function(){
-        Route::get('/contact', 'index')->name('index');
-        Route::post('/send', 'sendContact')->name('send');
-        Route::get('/admin/all', 'getAllContacts')->name('all');
-        Route::get('/admin/delete/{contact}', 'delete')->name('delete');
-    });
+Route::controller(ContactController::class)
+    ->name('contact.')
+    ->group(function(){
+        Route::get('/contact', 'index')
+            ->name('index');
+        Route::post('/send', 'sendContact')
+            ->name('send');
+        Route::get('/admin/all', 'getAllContacts')
+            ->name('all');
+        Route::get('/admin/delete/{contact}', 'delete')
+            ->name('delete');
+});
 
-    Route::controller(ProductsController::class)->prefix('/products')->name('products')->group(function() {
+Route::controller(ProductsController::class)
+    ->prefix('/products')
+    ->name('products.')
+    ->group(function() {
         Route::get('/all','index')
             ->name('all');
         Route::get('/delete/{product}','delete')
@@ -33,6 +42,6 @@ use Illuminate\Support\Facades\Route;
             ->name('edit');
         Route::post('/edit/save/{product}','editProduct')
             ->name('edit.save');
-    });
+});
 
 require __DIR__.'/auth.php';
